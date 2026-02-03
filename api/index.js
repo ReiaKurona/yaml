0b20a1e9d9212496174e3f25132c0e6bbfb3655b
ReiaKurona <z19906567940@gmail.com>
2026-02-04 00:23:28 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ const DEFAULT_CONFIG = {
     
     // 负载均衡组
     lbGroups: [
-        { name: "🇭🇰 香港", regex: "HK|hong|🇭🇰|IEPL" },
+        { name: "🇭🇰 香港", regex: "HK|hong|🇭🇰" },
         { name: "🇯🇵 日本", regex: "JP|japan|🇯🇵" },
         { name: "🇨🇦 加拿大", regex: "CA|canada|🇨🇦" }
     ],
